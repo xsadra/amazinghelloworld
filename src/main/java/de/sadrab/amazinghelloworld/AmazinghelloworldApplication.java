8f7526b0e1b0be6d7945232c8bf89d2bfd06f9bc
Sadra <ti9mail@gmail.com>
2018-03-21 16:14:28 +0100

    Add Spring

---
+++ src/main/java/de/sadrab/amazinghelloworld/AmazinghelloworldApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.amazinghelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinghelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinghelloworldApplication.class, args);
+	}
+}
